d69642a78820d4bf1d0f71c563dc9736cca255e2
SeoHee3478 <seohee3478@gmail.com>
2022-09-05 18:01:07 +0900

    feat: start 버튼 및 배경 생성

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+const score = document.querySelector(".score");
+const startBtn = document.querySelector(".startBtn");
+const gameArea = document.querySelector(".gameArea");
+const gameMessage = document.querySelector(".gameMessage");
+
+startBtn.addEventListener("click", start);
+gameMessage.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+let keys = {};
+
+function start() {
+  console.log("game start");
+  gameMessage.classList.add("hide");
+  startBtn.classList.add("hide");
+}
+
+function pressOn(e) {
+  console.log(e.code);
+  keys[e.code] = true;
+  console.log(keys);
+}
+
+function pressOff(e) {
+  console.log(e.code);
+  keys[e.code] = false;
+  console.log(keys);
+}
