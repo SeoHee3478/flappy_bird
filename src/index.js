e576b6cd7e993012f329455b166966ed4796a97a
SeoHee3478 <seohee3478@gmail.com>
2022-09-06 18:04:15 +0900

    feat: 장애물 만들기

--- src/index.js
+++ src/index.js
@@ -17,12 +17,18 @@ let player = {
   score: 0,
   inplay: false,
 };
+let pipe = {
+  startPos: 0,
+  spaceBetweenRow: 0, /*기둥과 기둥의 사이*/
+  spaceBetweenCol: 0, /*윗 기둥과 아랫기둥 사이의 간격*/
+  pipeCount: 0,
+};
 
 function start() {
   console.log("game start");
   player.inplay = true;
   player.score = 0;
-  gameArea.innerHTML ="";
+  gameArea.innerHTML = "";
   gameMessage.classList.add("hide");
   startBtn.classList.add("hide");
   let bird = document.createElement("div");
@@ -39,9 +45,47 @@ function start() {
   //bird의 left 값을 갖는다
   player.y = bird.offsetTop;
   //bird의 Top 값을 갖는다
+
+  pipe.startPos = 0;
+  pipe.spaceBetweenRow = 400;
+  pipe.pipeCount = Math.floor(
+    gameArea.offsetWidth / pipe.spaceBetweenRow
+  ); /*소수점 아래자리수는 버림*/
+  for (let i = 0; i < pipe.pipeCount; i++) {
+    makePipe(pipe.startPos * pipe.spaceBetweenRow);
+    pipe.startPos++;
+  }
+  //화면의 넓이에 따라 파이프의 개수를 동적으로 만들어준 것임//
+
   window.requestAnimationFrame(playGame);
   //어떤 움직임이있을 것인지 미리 알려줘서 부드럽게 처리해줌
-  
+}
+
+function makePipe(pipePos) {
+  let totalHeight = gameArea.offsetHeight;
+  let totalWidth = gameArea.offsetWidth;
+  let pipeUp = document.createElement("div");
+  pipeUp.classList.add("pipe");
+  pipeUp.height = Math.floor(Math.random() * 350);
+  pipeUp.style.height = pipeUp.height + "px";
+  pipeUp.style.left =
+    totalWidth + pipePos + "px"; /*처음 시작될 땐 화면밖에 있음*/
+  pipeUp.x = totalWidth + pipePos;
+  pipeUp.style.top = "0px";
+  pipeUp.style.backgroundColor = "red";
+
+  gameArea.appendChild(pipeUp);
+
+  pipe.spaceBetweenCol = Math.floor(Math.random() * 250) + 150;
+  let pipeDown = document.createElement("div");
+  pipeDown.classList.add("pipe");
+  pipeDown.style.height =
+    totalHeight - pipeUp.height - pipe.spaceBetweenCol + "px";
+  pipeDown.style.left = totalWidth + pipePos + "px";
+  pipeDown.x = totalWidth + pipePos;
+  pipeDown.style.bottom = "0px";
+  pipeDown.style.backgroundColor = "black";
+  gameArea.appendChild(pipeDown);
 }
 
 function playGame() {
